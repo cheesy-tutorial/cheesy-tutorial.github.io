f1566451ba7078ca7d4fbcef0951b585e19a0b4e
dev-gwang <pki054@naver.com>
2021-08-24 00:49:05 +0900

    이름 수정

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
   gatsby: {
     pathPrefix: '/develop',
-    siteUrl: 'https://cheesy.github.io/develop',
+    siteUrl: 'https://cheesy-tutorial.github.io/develop',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
     logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://cheesy.github.io/develop',
+    logoLink: 'https://cheesy-tutorial.github.io/develop',
     title:
-      "<a href='https://cheesy.github.io/develop'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/cheesy/develop',
+      "<a href='https://cheesy-tutorial.github.io/develop'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: 'https://github.com/cheesy-tutorial/develop',
     helpUrl: '',
     tweetText: '',
     social: `<li>
@@ -43,7 +43,7 @@ const config = {
     collapsedNav: [
       '/community', // add trailing slash if enabled above
     ],
-    links: [{ text: 'cheesy', link: 'https://cheesy.github.io/develop' }],
+    links: [{ text: 'cheesy-tutorial', link: 'https://cheesy-tutorial.github.io/develop' }],
     frontline: false,
     ignoreIndex: true,
     title:
@@ -53,7 +53,7 @@ const config = {
     title: 'Gatsby Gitbook Boilerplate | Hasura',
     description: 'Documentation built with mdx. Powering hasura.io/learn ',
     ogImage: null,
-    docsLocation: 'https://github.com/cheesy/develop/tree/develop/content',
+    docsLocation: 'https://github.com/cheesy-tutorial/develop/tree/develop/content',
     favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
   },
   pwa: {
